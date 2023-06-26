9545453ab3c755d00e1067bca208257e06ccce6d
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-26 18:43:18 +0300

    telegram bot and vercel config added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const TelegramBot = require('node-telegram-bot-api');
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
@@ -6,7 +7,11 @@ const masters = require("./routes/master.routes");
 const services = require("./routes/service.routes");
 const corsMiddleware = require("./middleware/cors.middleware");
 
+const token = config.get("telegramBotToken");
+const webAppUrl = config.get("webAppUrl");
+
 const app = express();
+const bot = new TelegramBot(token, {polling: true});
 const PORT = config.get("serverPort");
 
 app.use(corsMiddleware);
@@ -15,6 +20,21 @@ app.use(express.json());
 app.use("/api/", receptions);
 app.use("/api/", masters);
 app.use("/api/", services);
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if(text === '/start') {
+      await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+          reply_markup: {
+              inline_keyboard: [
+                  [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+              ]
+          }
+      })
+  }
+});
 
 const start = async () => {
   try {
