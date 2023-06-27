8ef1f942a58f866ec4307f8d5fbe342d49910d81
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-27 11:33:22 +0300

    updated cors middleware

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,12 +1,6 @@
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("optionsSuccessStatus", 200);
   res.header("Access-Control-Allow-Origin", "*");
-  if (req.method === "OPTIONS") {
-    res.send("");
-  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }
 
