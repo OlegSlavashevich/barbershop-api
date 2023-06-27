6f23bbd1b10c0450619041d23838710225065872
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-27 14:52:21 +0300

    updated send message

--- controllers/reception.controller.js
+++ controllers/reception.controller.js
@@ -87,7 +87,7 @@ class ReceptionController {
         id: queryId,
         title: 'Успешная покупка',
         input_message_content: {
-            message_text: `${req.body.client}, вы записались в барбешоп на время: ${startTime}`
+            message_text: `${req.body.client.name}, вы записались в барбешоп на время: ${startTime}`
         }
       })
 
